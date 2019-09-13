4b47c29d411a4db94d4f68da843637c9b6fd8c0a
DUBRUC Bryan <bryan.dubruc@etu.univ-amu.fr>
2019-09-13 11:29:15 +0200

    Correc 1 Exo 1

--- TD1.php
+++ TD1.php
@@ -1,7 +1,7 @@
 <?php
-    $var1 = 6;
-    $var2 = 1.3;
-    $var3 = 'Variable 3';
+$var1 = 6;
+$var2 = 1.3;
+$var3 = 'Variable 3';
 ?>
 
 <?php echo "$var1 + $var2";?>
@@ -9,6 +9,6 @@
 <?php echo $var1 + $var3;?>
 
 <?php
-    echo '</strong> Voici mon premier programme PHP </strong><br/>'."/n";
-    echo 'C \'est pas mal non ? <br/>';
+echo '<br/></strong> Voici mon premier programme PHP </strong><br/>'."\n";
+echo 'C \'est pas mal non ? <br/>';
 ?>
